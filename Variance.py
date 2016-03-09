c7a8528f56e5368e0a590459c8091f543ce9c4c2
Justin Gabriel Reid <justingreid@email.arizona.edu>
2016-03-09 04:04:40 -0700

    Altered Variance.py so it can be used for dictionaries and list

--- Variance.py
+++ Variance.py
@@ -1,31 +1,6 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-#  Variance.py
-#  
-#  Copyright 2016 Justin <Justin@PROMETHEUS>
-#  
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#  
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#  
-#  
-
-
-def main(args):
-    return 0
-    laps = {
+#!/usr/bin/python
+def main():
+	laps = {
 		0: 1,
 		1: 3,
 		2: 3,
@@ -34,19 +9,31 @@ def main(args):
 		6: 6,
 		7: 2
     }
-    print variance(laps)
-def varaince(d):
+	laps2 = [0,1,1,1,2,2,2,4,4,4,4,4,5,5,5,5,5,5,5,5,6,6,6,6,6,6,7,7]
+	print variance(laps)
+	print variance(laps2)
+
+def variance(d):
 	sum_of_vector = 0
-	var= 0
+	var = 0
 	length = 0
-	for key in d:
-		sum_of_vector = sum(key*d[key])
-	for key in d:
-		length = sum(d[key])
-	for key in d:
-		var = sum(d[key]*(key - average)**2) / (length - 1)
-	return var
-	
+	average = 0
+	if type(d) is dict:
+		for key in d:
+			sum_of_vector += key*d[key]
+		for key in d:
+			length += d[key]
+		average = sum_of_vector/ float(length)
+		for key in d:
+			var += (d[key]*(key - average)**2) / (length - 1)
+		return var
+	elif type(d) is list:
+		length = len(d)
+		for index in d: 
+			sum_of_vector += index
+		average = sum_of_vector / float(length)
+		for index in d: 
+			var += ((index - average)**2 / (length - 1))
+		return var
 if __name__ == '__main__':
-    import sys
-    sys.exit(main(sys.argv))
+	main()
