9e9bb27511ad81cf630c30d7f6554ba2e0512eab
Justin Gabriel Reid <justingreid@email.arizona.edu>
2016-03-09 03:06:49 -0700

    added main function and control structures

--- add_digits.py
+++ add_digits.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python
 
-print "Hello this program will add the digits of an integer until a singular digit is obtained"
-print 'For example 38 -> 3+8 = 11 -> 1+1 = 2. 2 will be returned'
-
-number = raw_input('Enter an integer value: ')
-#print type(number)
-
+def main():
+	print "Hello this program will add the digits of an integer until a singular digit is obtained"
+	print 'For example 38 -> 3+8 = 11 -> 1+1 = 2. 2 will be returned'
+	number = raw_input('Enter an integer value: ')
+	print addDigits(number)
 
 def addDigits(num):
 	num_string = num
@@ -20,16 +19,9 @@ def addDigits(num):
 			return int(num_s)			
 	else:
 		return int(num_string)
-	'''
-	while len(num_string) > 1:
-		for n in num_string:
-			sum_of_num += int(n)
-		num_string = str(sum_of_num)
-	else:
-		return int(num_string)
-	return int(num_string)
-	'''
-print addDigits(number)
+	
+if __name__ == '__main__':
+	main()
 
 
 
