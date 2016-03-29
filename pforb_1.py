29dff25f5b5b1f3ce93d8ae46f2d37961ebada90
justingabrielreid <justingreid@email.arizona.edu>
2016-03-29 00:45:30 -0700

    New files from programming for biology exersizes

---
+++ pforb_1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+#Programming for Biology Problem Set 1 Python Style 
+
+#Write a script to print out Hello New York
+
+print 'Hello New York'
+
+#store your name in a variable and print the contents 
+
+name = 'Justin'
+
+print name 
+
+#Create a print statement with tabs and new lines 
+
+print '\t'
+print ' ' 
+
+print (2 + 3)
+
---
+++ pforb_2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+#Programming for Biology Programming 2 - Python Style 
+
+#! 
+
+class Positive(object) :
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+	def pos(self):
+		if self.x >= 0 and self.y >= 0:
+			return True 
+		else:
+			print 'Please provide two positive numbers'
+class Adding(Positive):
+	def add(self):
+		return int(self.x) + int(self.y)
+			
+def main():
+	number1 = raw_input('Enter a integer: ')
+	number2 = raw_input('Enter another integer: ')
+	posornot = Adding(number1, number2)
+	print posornot.pos()
+	print posornot.add()
+	
+
+if __name__ == '__main__':
+	main()
+	
+	
+			
+
+			
