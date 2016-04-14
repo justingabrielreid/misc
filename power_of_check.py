82d5bbf4f8c15977f9a8cb0e382558d745f2d87c
justingabrielreid <justingreid@email.arizona.edu>
2016-04-14 11:32:41 -0700

    Fixed the power checker program it should run now

--- power_of_check.py
+++ power_of_check.py
@@ -1,23 +1,20 @@
-#1/usr/bin/python 
+#!/usr/bin/python 
 
-''' This program will check and see if an entered integer is a power of the specified base integer. For example n1 = 464 can be checked to see if it is a power of 2. If it is then the power will be returned. If not then the program will evalute it to be false.
-'''
-import math
-class Power_of(object):
-	def __init__(self, integer, power):
-		self.integer = integer
-		self.power = power
-	def checker(self):
-		if self.integer % self.power == 0:
-			return math.log(self.integer, self.power)
-		else:
-			return False
-			
+#will check and see if the number is a power of the specified base
+
+import math 
+
+def isPower(num, base):
+	if base == 0 and num == 0: return True
+	if base != 1 and num == 1: return False 
+	if base == 0 and num != 1: return False
+	power = math.log(num, base)
+	return (base ** power == num)
 
 def main():
-	number = int(input('Enter an integer: '))
-	base = int(input('Enter the base: '))
-	print (Power_of(number, base).checker())
+	x = int(input('Enter an integer: '))
+	y = int(input('Enter the base: '))
+	print (isPower(x , y))
 	
 	
 if __name__ == '__main__':
