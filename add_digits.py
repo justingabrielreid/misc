ab12a114d2cf00e6243631a2eb6380ffe3ddec6d
justin <justingreid@email.arizona.edu>
2016-04-14 20:35:08 -0700

    Fixed

--- add_digits.py
+++ add_digits.py
@@ -1,24 +1,20 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 def main():
-    print "Hello this program will add the digits of an integer until a singular digit is obtained"
-    print 'For example 38 -> 3+8 = 11 -> 1+1 = 2. 2 will be returned'
-    number = raw_input('Enter an integer value: ')
-    print addDigits(number)
+	print ("Hello this program will add the digits of an integer until a singular digit is obtained")
+	print ('For example 38 -> 3+8 = 11 -> 1+1 = 2. 2 will be returned')
+	number = input('Enter an integer value: ')
+	print (number + ' will reduce to ' + str(addDigits(number)))
 
 def addDigits(num):
-    num_string = num
-    sum_of_num = 0
-    if len(num_string) > 1:
-    	for n in num_string:
-    		sum_of_num += int(n)
-    	num_s = str(sum_of_num)
-    	if len(num_s) > 1:
-    		addDigits(num_s)
-    	else:
-    		return int(num_s)			
-    else:
-    	return int(num_string)
+	sum_of_num = 0
+	if len(num) == 1:
+		return num 
+	else: 
+		for n in str(num):
+			sum_of_num += int(n)
+		return addDigits(str(sum_of_num))
+
     
 if __name__ == '__main__':
     main()
