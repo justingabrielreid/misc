6a79aa05c9bc59b73325170971ec68ae031147f6
justin <justingreid@email.arizona.edu>
2016-04-14 18:50:06 -0700

    Fixed error methods, and added a while loop for input

--- power_of_check.py
+++ power_of_check.py
@@ -1,21 +1,25 @@
-#!/usr/bin/python 
+#!/usr/bin/python3 
 
 #will check and see if the number is a power of the specified base
 
 import math 
 
 def isPower(num, base):
+	#exceptions that will cause the code to fail if they are allowed to be
+	# called by the math.log function
+	# 0 and 1 bases and numbers are problematic
 	if base == 0 and num == 0: return True
-	if base != 1 and num == 1: return False 
-	if base == 0 and num != 1: return False
-	power = math.log(num, base)
+	if base == 0 and num != 0: return False 
+	if base != 0 and num == 0: return False 
+	if base == 1 and num == 1: return True
+	if base == 1 and num != 0: return True 
+	power = int(math.log(num, base))
 	return (base ** power == num)
 
 def main():
-	x = int(input('Enter an integer: '))
-	y = int(input('Enter the base: '))
-	print (isPower(x , y))
-	
-	
+	while True:
+		x = int(input('Enter an integer: '))
+		y = int(input('Enter the base: '))
+		print (isPower(x , y))
 if __name__ == '__main__':
 	main()
