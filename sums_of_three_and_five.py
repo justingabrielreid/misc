15ec6b96879f0732a003e43fcb12ff9a3b996039
justin <justingreid@email.arizona.edu>
2016-04-21 10:52:31 -0700

    new problem from project euler

---
+++ sums_of_three_and_five.py
@@ -0,0 +1,13 @@
+#!/usr/env python3 
+
+#Find the sum of numbers from 3 upto 1000 that are multiples of 3 and 5. 
+
+multiples = []
+
+numbers = range(3,1000)
+for num in numbers:
+	if ((num % 3 == 0) | (num % 5 == 0)):
+		multiples.append(num)
+print('The sum is: ' + str(sum(multiples)))
+print('There are this many multiples of three and five: ' + str(len(multiples)))
+
