33fdbba9e4bb81fe2d5e53c2e9802ceca237e06c
Justin_Reid <Prometheus@dhcp-10-132-201-58.uawifi.arizona.edu>
2016-04-28 10:43:35 -0700

    fixed the presidents analysis script so that the counter runs correctly, next steps are to alter the script so it can find the maximum number of presidents alive automatically

--- presidents_analysis.py
+++ presidents_analysis.py
@@ -1,61 +1,48 @@
-#!/usr/bin/python 
+#!/usr/bin/python
 #simple problem: find the year that the most presidents
 #were alive
-import pandas as pd 
-import numpy as np 
+import pandas as pd
+import numpy as np
 from collections import Counter
-#import the presidents.csv and save as a dataframe 
+#import the presidents.csv and save as a dataframe
 presidents = pd.read_csv('presidents.csv')
-#view the first ten lines of the dataframe 
+#view the first ten lines of the dataframe
 presidents.head(10)
-#change the column names to remove whitespace 
+#change the column names to remove whitespace
 presidents.columns = ['President','Birth Date','Birth Place','Death Date','Location of Death']
 #create a data frame that contains just the name, birth and death date of the president
 birth_and_death = presidents[['President','Birth Date','Death Date']]
 #now modify this so the Birth Date and Death Date are
 # integer values
-# 1st create a list for the birth year 
+# 1st create a list for the birth year
 birth_year =  list(birth_and_death['Birth Date'])
 #then remove cut the string till you get to the year
 birth_year[:] = [year.split(' ')[-1] for year in birth_year]
-#then assign this to the dataframe 
+#then assign this to the dataframe
 birth_and_death['Birth Date'] = birth_year
-#repeat the steps for the Death Date 
+#repeat the steps for the Death Date
 death_year = list(birth_and_death['Death Date'])
 death_year[:] = [year.split(' ')[-1] for year in death_year]
 birth_and_death['Death Date'] = death_year
-#now change the year to an integer 
+#now change the year to an integer
 birth_and_death['Birth Date'] = birth_and_death['Birth Date'].astype(int)
 #cannot repeat the same steps for the Death year as
-#some of the presidents are still alive 
-# I will use not really an optimal solution but I will 
-# assign the death year to the current year 
-# for the sake of simplicity 
+#some of the presidents are still alive
+# I will use not really an optimal solution but I will
+# assign the death year to the current year
+# for the sake of simplicity
 birth_and_death['Death Date'][(birth_and_death['Death Date'] == '')] = '2016'
 #now convert this column to integers
 birth_and_death['Death Date'] = birth_and_death['Death Date'].astype(int)
-#make a list of the possible years that presidents could
-# be living 
-yearCount = Counter()
-#counter for years that the presidents have lived! 
-for pres in birth_and_death:
-	for bday in birth_and_death['Birth Date']:
-		birthYear = bday
-	for dday in birth_and_death['Death Date']:
-		deathYear = dday
-	for year in range(birthYear, deathYear):
+#make a list of the possible years that presidents could be living
+#change the columns of birth_and_death to something easier to work with
+birth_and_death.columns = ['Presidents', 'Birth Year', 'Death Year']
+#now need these two list with the complete integer values of birth and death years
+birth_year = list(birth_and_death['Birth Year'])
+death_year = list(birth_and_death['Death Year'])
+#counter for years that the presidents have lived!
+for bday, dday in zip(birth_year, death_year):
+	for year in range(bday, dday):
 		yearCount.update({year})
-#currently not working as each year is coming out as
-#having three presidents living. 
-
-
-
-
-
-
-
-
-
-
- 
-
+#maximum number of presidents living in a single year was 18 presidents
+#those years include 1833, 1834, 1835, 1837, 1338, 1839, 1840, 1844,
